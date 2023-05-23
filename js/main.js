41b315e2233b311ecc9e7c517daf5b3a3c2a2682
KelsonTavares <kelsontavares123456@gmail.com>
2023-05-23 10:49:30 +0100

    added and altered some files.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,6 @@ if(preloader){
   })
 }
 
-
-/*let co = document.getElementById('comentarios');
-if(co){
-  window.addEventListener('click', ()=>{
-    co.remove();
-  })
-}*/
-// Inicializando o carousel
-//const carousel = new bootstrap.Carousel('#carousel-jogos');
-
 // Dropdown 
 var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
@@ -73,52 +63,46 @@ burger.addEventListener("click", () => {
 var btn_enviar = document.querySelector('#btn-enviar');
 
 btn_enviar.addEventListener('click', () =>{
-  // Criando elementos
   var name = document.querySelector('#nome');
   var email = document.querySelector('#email');
   var message = document.querySelector('#message');
-  //var opinions = document.querySelectorAll('.opnions');
 
 
-  var section_contacto = document.querySelector('#contacto');
-  var comentario = document.createElement("div");
-  var n_comentario = document.createElement("h5");
-  var e_comentario = document.createElement("h6");
-  var m_comentario = document.createElement("p");
+  if(name.value.length > 0 && email.value.length > 0 && message.value.length > 0){
+    var section_contacto = document.querySelector('#contacto');
+    // Criando elementos
+    var comentario = document.createElement("div");
+    var n_comentario = document.createElement("h5");
+    var e_comentario = document.createElement("h6");
+    var m_comentario = document.createElement("p");
 
-  comentario.classList = "opinion bg-dark p-2 align-items-center text-light border";
-  n_comentario.classList = "text-light";
-  e_comentario.classList = "text-light";
+    // Atribuindo classes nos elementos criados
+    comentario.classList = "opinion bg-dark p-2 align-items-center text-light border";
+    n_comentario.classList = "text-light";
+    e_comentario.classList = "text-light";
 
-  // Adicionar texto nas tags
-  n_comentario.innerText = name.value;
-  e_comentario.innerText = email.value;
-  m_comentario.innerText = message.value;
+    // Adicionar texto nas tags
+    n_comentario.innerText = name.value;
+    e_comentario.innerText = email.value;
+    m_comentario.innerText = message.value;
 
-  // Adicionar no arquivo json
-  //gravarDados(name.value, email.value, message.value);
-  // Adicionar tag comentario
-  comentario.appendChild(n_comentario);
-  comentario.appendChild(e_comentario);
-  comentario.appendChild(m_comentario);
-  //console.log(comentario.childElementCount);
-  // Adicionar a tag section
-  section_contacto.appendChild(comentario);
-  //console.log(opinion.length);
-  //console.log(section_contacto.childElementCount-1);
+    // Adicionar tag comentario
+    comentario.appendChild(n_comentario);
+    comentario.appendChild(e_comentario);
+    comentario.appendChild(m_comentario);
+    //console.log(comentario.childElementCount);
+    // Adicionar a tag section
+    section_contacto.appendChild(comentario);
 
-  
+    // Eliminando os dados dos inputs
+    name.value = "";
+    email.value = "";
+    message.value = "";
+    console.log('Ola')
 
-  name.value = "";
-  email.value = "";
-  message.value = "";
+  }
 
-  //if(section_contacto.childElementCount-1 > 3 ){
-    //comentarios();
-  //}
 });
-
-// Comentarios
 
 fetch("/js/comentario.json").then((response)=>{
   response.json().then((dados)=>{
